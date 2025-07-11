3e94c8410a3967ac903e620bd4b5e4f535565816
artem <artem006661artem@gmail.com>
2025-07-11 02:08:45 +0200

    debugging

--- src/main/java/com/example/localnews/LocalNewsApplication.java
+++ src/main/java/com/example/localnews/LocalNewsApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LocalNewsApplication {
 
     public static void main(String[] args) {
+        System.out.println("DB_URL = " + System.getenv("DB_URL"));
+        System.out.println("DB_USERNAME = " + System.getenv("DB_USERNAME"));
+        System.out.println("DB_PASSWORD = " + System.getenv("DB_PASSWORD"));
         SpringApplication.run(LocalNewsApplication.class, args);
     }
 
